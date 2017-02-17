40b87e2bf64ff3688dcc4e162d0fb334e5545cd0
fangmingzheng <fangming89@gmail.com>
2017-02-16 19:38:55 -0500

    chapter 5 exercise 01

---
+++ Chapter05-Ex01/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  Chapter5
+//
+//  Created by Fangming Zheng on 2/16/17.
+//  Copyright © 2017 Fangming Zheng. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int i = 0;
+    while ( i < 10 )
+    {
+        cout << i << '\n';
+        i++;
+    }
+}
