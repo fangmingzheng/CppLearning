091d5fbc2d177ff947a9a72f913c7f10838470de
fangmingzheng <fangming89@gmail.com>
2017-02-19 15:42:22 -0500

    Enum function

---
+++ Chapter7Exercise02/Chapter7Exercise02/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  Chapter7Exercise02
+//
+//  Created by Fangming Zheng on 2/19/17.
+//  Copyright © 2017 Fangming Zheng. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv [])
+{
+    enum RainbowColor
+    {
+        RC_RED ,
+        RC_ORANGE,
+        RC_YELLOW,
+        RC_GREEN,
+        RC_BLUE,
+        RC_INDIGO,
+        RC_VIOLET,
+        
+    };
+    std::cout << "Section: ";
+    
+}
