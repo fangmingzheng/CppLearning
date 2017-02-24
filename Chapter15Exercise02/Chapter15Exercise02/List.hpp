0ffd1bdc8806a3c2779bff24787f5f27d51a0a5f
fangmingzheng <fangming89@gmail.com>
2017-02-24 14:37:49 -0500

    Linked list

--- Chapter15Exercise02/Chapter15Exercise02/List.hpp
+++ Chapter15Exercise02/Chapter15Exercise02/List.hpp
@@ -10,5 +10,26 @@
 #define List_hpp
 
 #include <stdio.h>
-
+class List {
+private:
+    struct node {
+        int data;
+        node* next;
+    };
+    
+typedef struct node* nodePtr;
+    
+    nodePtr head;
+    nodePtr curr;
+    nodePtr temp;
+    
+    
+public: // this is where the functions go
+    
+    List ();
+    void AddNode(int addData);
+    void DeleteNode (int delData);
+    void PrintList();
+};
+    
 #endif /* List_hpp */
--- Chapter15Exercise02/Chapter15Exercise02/main.cpp
+++ Chapter15Exercise02/Chapter15Exercise02/main.cpp
@@ -6,10 +6,21 @@
 //  Copyright © 2017 Fangming Zheng. All rights reserved.
 //
 
-#include <iostream>
+#include <cstdlib>
+#include "List.hpp"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main (int argc, const char * argv[])
+{
+    List Rachel;
+    
+    Rachel.AddNode(3);
+    Rachel.AddNode(5);
+    Rachel.AddNode(7);
+    Rachel.PrintList();
+    
+    
+    Rachel.DeleteNode(3);
+    Rachel.PrintList();
+    
     return 0;
 }
