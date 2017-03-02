3c3428ff8caf3d51d7b049ee292754d9474b7dfe
fangmingzheng <fangming89@gmail.com>
2017-03-02 10:13:20 -0500

    recursion

--- Chapter16Exercise01/Chapter16Exercise01/main.cpp
+++ Chapter16Exercise01/Chapter16Exercise01/main.cpp
@@ -8,8 +8,19 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+void printNum(int num)
+{
+    cout << num;
+    if (num < 9 )
+    {
+        printNum (num + 1);
+    }
+    cout << num;
+}
+
+int main ()
+{
+    printNum(1);
     return 0;
 }
